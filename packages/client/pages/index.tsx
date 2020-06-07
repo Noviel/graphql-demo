af6628f3fea00b1fc73182f1b04f7a2e7a8869d0
Alexander Snov <snov.contact@gmail.com>
2020-06-08 03:53:47 +0700

    client: add graphql URL environment variable

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -3,7 +3,7 @@ import { GetStaticProps, NextPageContext } from 'next';
 type Props = NextPageContext;
 
 export default function IndexPage(props: Props) {
-  return <div>index page</div>;
+  return <div>index page, api: {process.env.NEXT_PUBLIC_GRAPHQL_URI}</div>;
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
